745270fec8331bfb1163e85c61d88b1de1c4622c
Daniel Cristian <danielcrisap@gmail.com>
2020-05-13 10:57:04 -0300

    Using List Comprehensions

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,22 +1,11 @@
 class Matrix:
     def __init__(self, matrix_string):
-        matrix_string = matrix_string.split("\n")
-        matrix_col = []
-        for item in matrix_string:
-            matrix_row = []
-            for element in item.split(" "):
-                matrix_row.append(int(element))
-            matrix_col.append(matrix_row)
-        matrix = matrix_col
-        self.matrix_string = matrix
+        self.matrix_string = [
+            [int(item) for item in col.split(" ")] for col in matrix_string.split("\n")
+        ]
 
     def row(self, index):
-        matrix = self.matrix_string
-        return matrix[index - 1]
+        return self.matrix_string[index - 1]
 
     def column(self, index):
-        matrix = self.matrix_string
-        col = []
-        for row in matrix:
-            col.append(row[index-1])
-        return col
+        return [i[index - 1] for i in self.matrix_string]
